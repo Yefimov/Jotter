04921c4ccea68c08a62af54ffa4a436fa7f3b7c5
Ilya Yefimov <is.Yefimov@gmail.com>
2017-02-25 22:39:19 +0400

    :suspect: Unit-tests filled it
    Created all unit-tests (exception: unit-tests for save'n'load).

--- JotterTest/AddDelete_Test.cs
+++ JotterTest/AddDelete_Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace JotterTest
 {
@@ -11,10 +12,23 @@ namespace JotterTest
         public void AddEmployeeTest()
         {
             // Arrange.
+            var expected = new Employee
+            {
+                id = 10,
+                surname = @"Smith",
+                forename = @"John",
+                yearOfBirth = 1995,
+                phoneNumber = @"+14075552368",
+                manager = @"Johnson Ethan"
+            };
+            var listEmployee = new List<Employee>();
 
             // Act.
+            AddEmployee(listEmployee, id, surname, forename, yearOfBirth, phoneNumber, manager);
+            var actual = listEmployee[0];
 
             // Assert.
+            Assert.AreEqual(expected, actual);
         }
 
         // Create an entry about new Manager
@@ -22,10 +36,23 @@ namespace JotterTest
         public void AddManagerTest()
         {
             // Arrange.
+            var expected = new Manager
+            {
+                id = 1,
+                surname = @"Johnson",
+                forename = @"Ethan",
+                yearOfBirth = 1980,
+                phoneNumber = @"+1407-555-4278",
+                departmentName = @"Testing Department"
+            };
+            var listManager = new List<Manager>();
 
             // Act.
+            AddManager(listManager, id, surname, forename, yearOfBirth, phoneNumber, departmentName);
+            var actual = listManager[0];
 
             // Assert.
+            Assert.AreEqual(expected, actual);
         }
 
         // Delete an entry about Employee
@@ -34,9 +61,20 @@ namespace JotterTest
         {
             // Arrange.
 
+            // Create an expected list of employees.
+            var expected = new List<Employee>();
+            // Add employees to the list.
+            expected.Add(new Employee() { id = 1, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+
+            var actual = new List<Employee>();
+            actual.Add(new Employee() { id = 10, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+            actual.Add(new Employee() { id = 11, surname = @"Gibson", forename = @"Patrick", yearOfBirth = 1993, phoneNumber = @"+19315558642", manager = @"Johnson Ethan" });
+            
             // Act.
+            DeleteEmployee(actual, actual[1].id);
 
             // Assert.
+            Assert.AreEqual(expected, actual);
         }
 
         // Delete an entry about Manager
@@ -44,10 +82,35 @@ namespace JotterTest
         public void DeleteManagerTest()
         {
             // Arrange.
+            
+            var expected = new List<Manager>();
+            expected.Add(new Manager() { id = 1, surname = @"Johnson", forename = @"Ethan", yearOfBirth = 1980, phoneNumber = @"" + 14075554278"", departmentName = @"Testing Department" });
+
+            var actual = new List<Manager>();
+            expected.Add(new Manager() { id = 1, surname = @"Johnson", forename = @"Ethan", yearOfBirth = 1980, phoneNumber = @"+1407-555-4278", departmentName = @"Testing Department" });
+            expected.Add(new Manager() { id = 5, surname = @"Mason", forename = @"Lawrence", yearOfBirth = 1984, phoneNumber = @"+1407-555-3727", departmentName = @"Testing Department" });
 
             // Act.
+            DeleteManager(actual, actual[1].id);
 
             // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Delete odd symbols from mobile number
+        [TestMethod]
+        public void CleanPhoneTest()
+        {
+            // Arrange.
+
+            var expected = @"+14075554278";
+            var input = @" +1-407-555-4278 ";
+
+            // Act.
+            var actual = CleanPhone(input);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- JotterTest/SortSearch_Test.cs
+++ JotterTest/SortSearch_Test.cs
@@ -11,10 +11,21 @@ namespace JotterTest
         public void SearchForenameTest()
         {
             // Arrange.
+            var listEmployee = new List<Employee>();
+            listEmployee.Add(new Employee() { id = 10, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Employee() { id = 12, surname = @"Schroeder", forename = @"John", yearOfBirth = 1996, phoneNumber = @"+14075559434", manager = @"Mason Lawrence" });
+            listEmployee.Add(new Employee() { id = 11, surname = @"Gibson", forename = @"Patrick", yearOfBirth = 1993, phoneNumber = @"+19315558642", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Manager() { id = 1, surname = @"Johnson", forename = @"Ethan", yearOfBirth = 1980, phoneNumber = @"+1407-555-4278", departmentName = @"Testing Department" });
+            listEmployee.Add(new Manager() { id = 5, surname = @"Mason", forename = @"Lawrence", yearOfBirth = 1984, phoneNumber = @"+1407-555-3727", departmentName = @"Testing Department" });
+            var expected = new List<Employee>();
+            listEmployee.Add(new Employee() { id = 10, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Employee() { id = 12, surname = @"Schroeder", forename = @"John", yearOfBirth = 1996, phoneNumber = @"+14075559434", manager = @"Mason Lawrence" });
 
             // Act.
+            var actual = SearchForename(listEmployee, @"John");
 
             // Assert.
+            Assert.AreEqual(expected, actual);
         }
 
         // This method is for searching a person by its surname.
@@ -22,10 +33,21 @@ namespace JotterTest
         public void SearchSurnameTest()
         {
             // Arrange.
+            var listEmployee = new List<Employee>();
+            listEmployee.Add(new Employee() { id = 10, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Employee() { id = 12, surname = @"Smith", forename = @"Michael", yearOfBirth = 1996, phoneNumber = @"+14075559434", manager = @"Mason Lawrence" });
+            listEmployee.Add(new Employee() { id = 11, surname = @"Gibson", forename = @"Patrick", yearOfBirth = 1993, phoneNumber = @"+19315558642", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Manager() { id = 1, surname = @"Johnson", forename = @"Ethan", yearOfBirth = 1980, phoneNumber = @"+1407-555-4278", departmentName = @"Testing Department" });
+            listEmployee.Add(new Manager() { id = 5, surname = @"Mason", forename = @"Lawrence", yearOfBirth = 1984, phoneNumber = @"+1407-555-3727", departmentName = @"Testing Department" });
+            var expected = new List<Employee>();
+            listEmployee.Add(new Employee() { id = 10, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Employee() { id = 12, surname = @"Smith", forename = @"Michael", yearOfBirth = 1996, phoneNumber = @"+14075559434", manager = @"Mason Lawrence" });
 
             // Act.
+            var actual = SearchSurename(listEmployee, @"Smith");
 
             // Assert.
+            Assert.AreEqual(expected, actual);
         }
 
         // This method is for searching a person by its phonenumber.
@@ -33,10 +55,20 @@ namespace JotterTest
         public void SearchPhoneTest()
         {
             // Arrange.
+            var listEmployee = new List<Employee>();
+            listEmployee.Add(new Employee() { id = 10, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Employee() { id = 12, surname = @"Schroeder", forename = @"John", yearOfBirth = 1996, phoneNumber = @"+14075559434", manager = @"Mason Lawrence" });
+            listEmployee.Add(new Employee() { id = 11, surname = @"Gibson", forename = @"Patrick", yearOfBirth = 1993, phoneNumber = @"+19315558642", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Manager() { id = 1, surname = @"Johnson", forename = @"Ethan", yearOfBirth = 1980, phoneNumber = @"+1407-555-4278", departmentName = @"Testing Department" });
+            listEmployee.Add(new Manager() { id = 5, surname = @"Mason", forename = @"Lawrence", yearOfBirth = 1984, phoneNumber = @"+1407-555-3727", departmentName = @"Testing Department" });
+            var expected = new List<Employee>();
+            listEmployee.Add(new Manager() { id = 5, surname = @"Mason", forename = @"Lawrence", yearOfBirth = 1984, phoneNumber = @"+1407-555-3727", departmentName = @"Testing Department" });
 
             // Act.
+            var actual = SearchPhone(listEmployee, @"+1407-555-3727");
 
             // Assert.
+            Assert.AreEqual(expected, actual);
         }
 
         // Sort all entries by their surnames.
@@ -44,10 +76,24 @@ namespace JotterTest
         public void SortSurnameTest()
         {
             // Arrange.
-
+            var listEmployee = new List<Employee>();
+            listEmployee.Add(new Employee() { id = 10, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Employee() { id = 12, surname = @"Schroeder", forename = @"John", yearOfBirth = 1996, phoneNumber = @"+14075559434", manager = @"Mason Lawrence" });
+            listEmployee.Add(new Employee() { id = 11, surname = @"Gibson", forename = @"Patrick", yearOfBirth = 1993, phoneNumber = @"+19315558642", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Manager() { id = 1, surname = @"Johnson", forename = @"Ethan", yearOfBirth = 1980, phoneNumber = @"+1407-555-4278", departmentName = @"Testing Department" });
+            listEmployee.Add(new Manager() { id = 5, surname = @"Mason", forename = @"Lawrence", yearOfBirth = 1984, phoneNumber = @"+1407-555-3727", departmentName = @"Testing Department" });
+            var expected = new List<Employee>();
+            listEmployee.Add(new Employee() { id = 11, surname = @"Gibson", forename = @"Patrick", yearOfBirth = 1993, phoneNumber = @"+19315558642", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Manager() { id = 1, surname = @"Johnson", forename = @"Ethan", yearOfBirth = 1980, phoneNumber = @"+1407-555-4278", departmentName = @"Testing Department" });
+            listEmployee.Add(new Manager() { id = 5, surname = @"Mason", forename = @"Lawrence", yearOfBirth = 1984, phoneNumber = @"+1407-555-3727", departmentName = @"Testing Department" });
+            listEmployee.Add(new Employee() { id = 12, surname = @"Schroeder", forename = @"John", yearOfBirth = 1996, phoneNumber = @"+14075559434", manager = @"Mason Lawrence" });
+            listEmployee.Add(new Employee() { id = 10, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+            
             // Act.
+            var actual = SortSurname(listEmployee);
 
             // Assert.
+            Assert.AreEqual(expected, actual);
         }
 
         // Sort all entries by their birthdays.
@@ -55,10 +101,24 @@ namespace JotterTest
         public void SortBirthdayTest()
         {
             // Arrange.
+            var listEmployee = new List<Employee>();
+            listEmployee.Add(new Employee() { id = 10, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Employee() { id = 12, surname = @"Schroeder", forename = @"John", yearOfBirth = 1996, phoneNumber = @"+14075559434", manager = @"Mason Lawrence" });
+            listEmployee.Add(new Employee() { id = 11, surname = @"Gibson", forename = @"Patrick", yearOfBirth = 1993, phoneNumber = @"+19315558642", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Manager() { id = 1, surname = @"Johnson", forename = @"Ethan", yearOfBirth = 1980, phoneNumber = @"+1407-555-4278", departmentName = @"Testing Department" });
+            listEmployee.Add(new Manager() { id = 5, surname = @"Mason", forename = @"Lawrence", yearOfBirth = 1984, phoneNumber = @"+1407-555-3727", departmentName = @"Testing Department" });
+            var expected = new List<Employee>();
+            listEmployee.Add(new Manager() { id = 1, surname = @"Johnson", forename = @"Ethan", yearOfBirth = 1980, phoneNumber = @"+1407-555-4278", departmentName = @"Testing Department" });
+            listEmployee.Add(new Manager() { id = 5, surname = @"Mason", forename = @"Lawrence", yearOfBirth = 1984, phoneNumber = @"+1407-555-3727", departmentName = @"Testing Department" });
+            listEmployee.Add(new Employee() { id = 11, surname = @"Gibson", forename = @"Patrick", yearOfBirth = 1993, phoneNumber = @"+19315558642", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Employee() { id = 10, surname = @"Smith", forename = @"John", yearOfBirth = 1995, phoneNumber = @"+14075552368", manager = @"Johnson Ethan" });
+            listEmployee.Add(new Employee() { id = 12, surname = @"Schroeder", forename = @"John", yearOfBirth = 1996, phoneNumber = @"+14075559434", manager = @"Mason Lawrence" });
 
             // Act.
+            var actual = SortBirthday(listEmployee);
 
             // Assert.
+            Assert.AreEqual(expected, actual);
         }
     }
 }
